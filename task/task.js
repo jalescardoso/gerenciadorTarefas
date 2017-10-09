af19080d98e10ba8747c456b209ecfaab3a1ad26
jalescardoso <jalescardoso@hotmail.com>
2017-10-09 12:44:28 -0300

    Update task.js

--- task/task.js
+++ task/task.js
@@ -3,18 +3,18 @@ angular.module('task').controller('tableController', function ($scope, $location
   $scope.tasks = srv.getData();
 
   $scope.delete = function () {
-    let selecionado = $scope.tasks.filter(function (el) { return el.selecionado });
+    let selecionado = $scope.tasks.filter(el => el.selecionado );
     if (selecionado.length === 0) {
       alert('selecione pelo menos um item');
     } else {
-      $scope.tasks = $scope.tasks.filter(function (el) { return !el.selecionado });
+      $scope.tasks = $scope.tasks.filter(el => !el.selecionado);
       Notify('Items deletados com sucesso', 'top-right', '5000', 'info', 'fa-warning', true);
       localStorage.setItem('tasks', JSON.stringify($scope.tasks));
     }
   }
 
   $scope.editar = function () {
-    let selecionado = $scope.tasks.filter(function (el) { return el.selecionado });
+    let selecionado = $scope.tasks.filter(el => el.selecionado );
     if (selecionado.length === 1) {
       $location.url('/update/' + selecionado[0].id);
     } else {
